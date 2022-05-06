949c8c02e1ea4056bd05d7d0d924a9a4b1944035
AlexeyVasilev0101 <101921598+AlexeyVasilev0101@users.noreply.github.com>
2022-05-06 14:09:30 +0300

    Ex1

---
+++ EX1OOP/Header.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+struct Complex
+{
+	double re = 0;
+	double im = 0;
+	void print();
+	Complex sum(Complex cmp1);
+	Complex mult(Complex cmp1);
+	Complex del(Complex cmp1);
+	double mod();
+};
